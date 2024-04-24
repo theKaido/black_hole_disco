afa476ef739b0521a4146b3e272a418b2d51beb7
theKaido <jonnymthdev@gmail.com>
2024-04-24 03:41:54 +0200

    try to add particle but its didn't work

--- window.c
+++ window.c
@@ -8,15 +8,15 @@
 #include <GL4D/gl4du.h>
 #include <GL4D/gl4df.h>
 #include <GL4D/gl4duw_SDL2.h>
-
+/* Prototypes des fonctions statiques contenues dans ce fichier C */
 #define NUM_PARTICLES 1000
 
 typedef struct {
     GLfloat position[3];
     GLfloat color[4];
+    GLuint sphereId;
 } Particle;
 
-/* Prototypes des fonctions statiques contenues dans ce fichier C */
 static void init(void);
 static void resize(int w, int h);
 static void draw(void);
@@ -26,7 +26,8 @@ static int _wW = 800, _wH = 600;
 /*!\brief identifiant du programme GLSL */
 static GLuint _pId = 0;
 /*!\brief quelques objets géométriques */
-static GLuint _sphere = 0, _quad = 0;
+static Particle particles[NUM_PARTICLES];
+
 /*!\brief La fonction principale créé la fenêtre d'affichage,
  * initialise GL et les données, affecte les fonctions d'événements et
  * lance la boucle principale d'affichage.*/
@@ -45,13 +46,25 @@ int main(int argc, char ** argv) {
 static void init(void) {
   glEnable(GL_DEPTH_TEST);
   glEnable(GL_CULL_FACE);
-  glClearColor(1.0f, 0.7f, 0.7f, 0.0f);
+  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
   _pId  = gl4duCreateProgram("<vs>shaders/dep3d.vs", "<fs>shaders/dep3d.fs", NULL);
   gl4duGenMatrix(GL_FLOAT, "modelViewMatrix");
   gl4duGenMatrix(GL_FLOAT, "projectionMatrix");
   resize(_wW, _wH);
-  _sphere = gl4dgGenSpheref(30, 30);
-  _quad = gl4dgGenQuadf();
+  for (int i = 0; i < NUM_PARTICLES; ++i) {
+      particles[i].position[0] = (GLfloat)(rand() % 20 - 10); // Position x aléatoire entre -10 et 10
+      particles[i].position[1] = (GLfloat)(rand() % 20 - 10); // Position y aléatoire entre -10 et 10
+      particles[i].position[2] = (GLfloat)(rand() % 20 - 10); // Position z aléatoire entre -10 et 10
+      particles[i].color[0] = (GLfloat)rand() / RAND_MAX; // Composante rouge aléatoire entre 0 et 1
+      particles[i].color[1] = (GLfloat)rand() / RAND_MAX; // Composante verte aléatoire entre 0 et 1
+      particles[i].color[2] = (GLfloat)rand() / RAND_MAX; // Composante bleue aléatoire entre 0 et 1
+      particles[i].color[3] = 1.0f; // Opacité à 1
+
+
+       particles[i].sphereId = gl4dgGenSpheref(30, 30);
+        
+  }
+  
 }
 /*!\brief Cette fonction paramétre la vue (viewport) OpenGL en
  * fonction des dimensions de la fenêtre.*/
@@ -65,36 +78,29 @@ static void resize(int w, int h) {
   gl4duBindMatrix("modelViewMatrix");
 }
 /*!\brief dessine dans le contexte OpenGL actif. */
+
 static void draw(void) {
-  static GLfloat a = 0;
-  GLfloat rouge[] = {1, 0, 0, 1}, vert[] = {0, 1, 0, 1};
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  gl4duBindMatrix("modelViewMatrix");
-  gl4duLoadIdentityf();
-  glUseProgram(_pId);
-  gl4duTranslatef(0, 0, -10.0);
-  gl4duPushMatrix(); {
-    gl4duTranslatef(0, -2.0, 0.0);
-    gl4duRotatef(-90, 1, 0, 0);
-    gl4duScalef(3.5f, 3, 3);
+    static GLfloat a = 0;
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    gl4duBindMatrix("modelViewMatrix");
+    gl4duLoadIdentityf(); // Réinitialise la matrice modèle à l'identité
+
+    glUseProgram(_pId);
+    gl4duTranslatef(0, 0, -25.0);
+    gl4duPushMatrix(); // Sauvegarde la matrice modèle
+    for (int i = 0; i < NUM_PARTICLES; ++i) {
+        printf("Particle %d - Position: (%f, %f, %f)\n", i, particles[i].position[0], particles[i].position[1], particles[i].position[2]); // Affiche les positions des particules
+        gl4duLoadIdentityf(); // Réinitialise la matrice modèle pour chaque particule
+        gl4duTranslatef(particles[i].position[0], particles[i].position[1], particles[i].position[2]);
+        glUniform4fv(glGetUniformLocation(_pId, "couleur"), 1, particles[i].color);
+        gl4dgDraw(particles[i].sphereId);
+    }
+    gl4duPopMatrix(); // Restaure la matrice modèle sauvegardée
     gl4duSendMatrices();
-  } gl4duPopMatrix();
-  glUniform4fv(glGetUniformLocation(_pId, "couleur"), 1, vert);
-  gl4dgDraw(_quad);
-
-  gl4duTranslatef(0, -1, 0);
-  gl4duSendMatrices();
-  glUniform4fv(glGetUniformLocation(_pId, "couleur"), 1, rouge);
-  gl4dgDraw(_sphere);
-  gl4duPopMatrix();
-  gl4dgDraw(_sphere);
-
-  /* Décommenter pour avoir un rendu sympathique pour pas cher :) */
-  /*   gl4dfBlur(0, 0, 5, 1, 0, GL_FALSE); */
-  /*   gl4dfSobelSetMixMode(GL4DF_SOBEL_MIX_MULT); */
-  /*   gl4dfSobel(0, 0, GL_FALSE); */
-  a++;
+    a++;
 }
+
+
 /*!\brief appelée au moment de sortir du programme (atexit), libère les éléments utilisés */
 static void quit(void) {
   gl4duClean(GL4DU_ALL);
